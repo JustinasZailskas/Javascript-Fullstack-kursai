a9677abcf579f3a0a3cfbd5287b1755444f111cd
JustinasZailskas <justinaszailskas@gmail.com>
2024-08-01 22:41:22 +0300

    Variables exercise

---
+++ JAVASCRIPT/variables/script.js
@@ -0,0 +1,80 @@
+// let firstname = "Jonas";
+// let lastname = "Jonaitis";
+// let age = 19;
+// let height = 1.85;
+// let weight = 80;
+// let school = "VIKO";
+// let group = "PI24E";
+// let course = 1;
+// let study_programme = "Software Enginering";
+// let credits = 30;
+
+// console.log("vardas ", firstname);
+// console.log("pavarde ", lastname);
+// console.log("metai ", age);
+// console.log("ugis ", height);
+// console.log("svoris ", weight);
+// console.log("aukstoji mokykla ", school);
+// console.log("grupe ", group);
+// console.log("Kursas ", course);
+// console.log("Studiju programa ", study_programme);
+// console.log("Kreditu skaicius ", credits);
+
+// let city_name = "Vilnius";
+// let country = "Lithuania";
+// let district = "Vilnius district";
+// let year = 1323;
+// let mayor = "Valdas Benkunskas";
+// let area = 401;
+// let population = 541505;
+
+// console.log(city_name);
+// console.log(country);
+// console.log(district);
+// console.log(year);
+// console.log(mayor);
+// console.log(area);
+// console.log(population);
+
+let my_name = "Justinas";
+console.log("Mano vardas yra ", my_name);
+let group_name = "ifzm-6";
+let study_average = 8;
+console.log("akademine grupe: ", group_name);
+console.log("vidurkis: ", study_average);
+let random_word =
+  "Lietuviu 3x3 krepsinio komanda nugalejo amerikieciu 3x3 komanda";
+console.log(random_word, random_word, random_word, random_word, random_word);
+
+let animal_type = "suo";
+let age = "3";
+let hair_color = "juoda";
+let weight = 26;
+console.log(
+  "Gyvunas - ",
+  animal_type,
+  " (",
+  age,
+  "m.)",
+  " turi ",
+  hair_color,
+  " kailio spalva ir sveria",
+  weight,
+  " kg."
+);
+console.log(
+  `Gyvunas - ${animal_type} (${age} m.) turi ${hair_color} kailio spalva ir sveria ${weight} kg.`
+);
+let number_value = 25;
+console.log(
+  number_value,
+  number_value,
+  number_value,
+  number_value,
+  number_value
+);
+console.log(
+  `${number_value}${number_value}${number_value}${number_value}${number_value}`
+);
+
+console.log("___________\n|         |\n|         |\n|         |\n___________");
